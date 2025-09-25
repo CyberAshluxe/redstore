de3ed40f9b89ca98f2d637fae1f5fc90f381abec
Ashrof <ashrofabdulrofiu21.com>
2025-09-25 11:03:33 +0100

    footer added to login

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Footer from "../components/Footer";
 
 const Signin = () => {
   const [email, setEmail] = useState("");
@@ -113,7 +114,9 @@ const Signin = () => {
           </div>
         </div>
       )}
+      <Footer />
     </div>
+    
   );
 };
 
